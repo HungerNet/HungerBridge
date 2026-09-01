ae7f2e149af48a02bbbce99f56f39d8969c65d2e
iFamished <me@ifamished.com>
2026-09-01 09:07:02 -0500

    Fix

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -4,3 +4,20 @@ plugins {
 
 // Version-specific Paper configuration is defined under paper/versions/*.
 // Keep the root project as a thin wrapper to avoid applying Paperweight twice.
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
+}
+
+dependencies {
+    implementation(project(":common"))
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("org.apache.logging.log4j:log4j-core:2.20.0")
+}
