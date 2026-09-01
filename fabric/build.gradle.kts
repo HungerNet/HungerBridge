bbdbaaff3953ec28a27915b855f27c79743a6b02
iFamished <me@ifamished.com>
2026-09-01 14:32:44 -0500

    Test

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -2,5 +2,7 @@ plugins {
     id("java")
 }
 
+version = "hungerbridge-${stonecutter.current.minecraftVersion}-${stonecutter.current.loader}+${stonecutter.current.version}"
+
 // Version-specific Fabric configuration is defined under fabric/versions/*.
 // Keep the root project as a thin wrapper to avoid applying Loom twice.
--- fabric/versions/26.1.2-fabric/build.gradle.kts
+++ fabric/versions/26.1.2-fabric/build.gradle.kts
@@ -1,12 +1,18 @@
 plugins {
-    id("fabric-loom") version "1.9.2"
+    id("fabric-loom") version "1.18.0-alpha.19"
 }
 
-// Artifact version derived from Stonecutter
-version = "hungerbridge-${'$'}{stonecutter.current.minecraftVersion}-${'$'}{stonecutter.current.loader}+${'$'}{stonecutter.current.version}"
+version = rootProject.version.toString()
 
 repositories {
     mavenCentral()
+    maven("https://maven.fabricmc.net/")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
 }
 
 dependencies {
